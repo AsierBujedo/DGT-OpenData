28c247299fde218d6de983276aad85438cb79566
asierBu <asier.bujedo@opendeusto.es>
2025-11-16 17:07:13 +0100

    feature: Create project structure

---
+++ src/main/java/com/dgt/opendata/models/Autoescuela.java
@@ -0,0 +1,12 @@
+package com.dgt.opendata.models;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Autoescuela {
+    
+    /*
+     * TODO:
+     * Implement the models.
+     */
+}
---
+++ src/main/java/com/dgt/opendata/service/Service.java
@@ -0,0 +1,19 @@
+package com.dgt.opendata.service;
+
+import com.dgt.opendata.service.queries.IQueries;
+import com.dgt.opendata.service.queries.Queries;
+
+public class Service {
+
+    private IQueries queries;
+
+    public Service() {
+        queries = new Queries();
+    }
+
+    /*
+     * TODO:
+     * Implement the service
+     */
+
+}
