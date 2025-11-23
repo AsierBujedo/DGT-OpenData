de5da8e2923a0cf8ad1c2b547a10624e6b0980aa
asierBu <asier.bujedo@opendeusto.es>
2025-11-23 14:55:56 +0100

    feat: Models setup

--- src/main/java/com/dgt/opendata/models/Autoescuela.java
+++ src/main/java/com/dgt/opendata/models/Autoescuela.java
@@ -1,12 +1,20 @@
 package com.dgt.opendata.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 
 @Entity
 public class Autoescuela {
-    
-    /*
-     * TODO:
-     * Implement the models.
-     */
+    @Column(name = "codigo", nullable = false)
+    String codigo;
+    @Column(name = "nombre", nullable = false)
+    String nombre;
+    @Column(name = "provincia", nullable = false)
+    String provincia;
+
+    public Autoescuela(String codigo, String nombre, String provincia) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.provincia = provincia;
+    }
 }
