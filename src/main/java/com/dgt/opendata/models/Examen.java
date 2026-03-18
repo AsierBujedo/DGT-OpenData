0df573dd8dc73a73f94df5e9be628e14c1792df8
asierBu <asier.bujedo@opendeusto.es>
2026-03-18 22:05:13 +0100

    fix

--- src/main/java/com/dgt/opendata/models/Examen.java
+++ src/main/java/com/dgt/opendata/models/Examen.java
@@ -4,22 +4,32 @@ public class Examen {
     private String nombre_autoescuela;
     private String provincia;
     private String seccion;
+    private String tipo_examen;
+    private String nombre_permiso;
     private Long numero_aptos;
     private Long numero_no_aptos;
     private Long numero_aptos_1;
     private Long numero_aptos_2;
     private Long numero_aptos_3o4;
     private Long numero_aptos_mas_5;
+    private String mes;
+    private String anyo;
 
-    public Examen(String nombre_autoescuela, String provincia, String seccion, Long numero_aptos, Long numero_no_aptos, Long numero_aptos_1, Long numero_aptos_2, Long numero_aptos_3o4, Long numero_aptos_mas_5) {
+    public Examen(String nombre_autoescuela, String provincia, String seccion, String tipo_examen, 
+                  String nombre_permiso, Long numero_aptos, Long numero_no_aptos, Long numero_aptos_1, 
+                  Long numero_aptos_2, Long numero_aptos_3o4, Long numero_aptos_mas_5, String mes, String anyo) {
         this.nombre_autoescuela = nombre_autoescuela;
         this.provincia = provincia;
         this.seccion = seccion;
+        this.tipo_examen = tipo_examen;
+        this.nombre_permiso = nombre_permiso;
         this.numero_aptos = numero_aptos;
         this.numero_no_aptos = numero_no_aptos;
         this.numero_aptos_1 = numero_aptos_1;
         this.numero_aptos_2 = numero_aptos_2;
         this.numero_aptos_3o4 = numero_aptos_3o4;
         this.numero_aptos_mas_5 = numero_aptos_mas_5;
+        this.mes = mes;
+        this.anyo = anyo;
     }
 }
