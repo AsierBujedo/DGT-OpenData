f8eb3a0c2badf5087027cb4d66c222c49853ac81
asierBu <asier.bujedo@opendeusto.es>
2026-04-12 17:47:20 +0200

    refactor

--- src/main/java/com/dgt/opendata/service/queries/Queries.java
+++ src/main/java/com/dgt/opendata/service/queries/Queries.java
@@ -1,132 +1,91 @@
 package com.dgt.opendata.service.queries;
 
-import javax.sql.DataSource;
-
 import org.springframework.stereotype.Repository;
 
+import com.dgt.opendata.service.utils.DBUtils;
+
+import javax.sql.DataSource;
 import java.io.IOException;
-import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Types;
 
 @Repository
 public class Queries implements IQueries {
-    
+
     private final DataSource dataSource;
 
     public Queries(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
-    public ResultSet executeQueryGetAutoescuelas(String codigo_autoescuela, String nombre_autoescuela, String provincia, String seccion) throws IOException, SQLException {
-        String sql = "{call [DGTOpendata].[dbo].[Autoescuelas_GetAutoescuela](?, ?, ?, ?)}";
-
-        Connection connection = dataSource.getConnection();
-
-        var stmt = connection.prepareCall(sql);
-
-        int paramIndex = 1;
-        if (codigo_autoescuela != null) {
-            stmt.setString(paramIndex++, codigo_autoescuela);
-        } else {
-            stmt.setNull(paramIndex++, java.sql.Types.VARCHAR);
-        }
-        if (nombre_autoescuela != null) {
-            stmt.setString(paramIndex++, nombre_autoescuela);
-        } else {
-            stmt.setNull(paramIndex++, java.sql.Types.VARCHAR);
-        }
-        if (provincia != null) {
-            stmt.setString(paramIndex++, provincia);
-        } else {
-            stmt.setNull(paramIndex++, java.sql.Types.VARCHAR);
-        }
-        if (seccion != null) {
-            stmt.setString(paramIndex++, seccion);
-        } else {
-            stmt.setNull(paramIndex++, java.sql.Types.VARCHAR);
-        }
-
-        return stmt.executeQuery();
+    @Override
+    public ResultSet executeQueryGetAutoescuelas(
+            String codigo_autoescuela,
+            String nombre_autoescuela,
+            String provincia,
+            String seccion
+    ) throws IOException, SQLException {
+        return DBUtils.executeStoredProcedure(
+            dataSource,
+            "[DGTOpendata].[dbo].[Autoescuelas_GetAutoescuela]",
+            DBUtils.param(codigo_autoescuela, Types.VARCHAR),
+            DBUtils.param(nombre_autoescuela, Types.VARCHAR),
+            DBUtils.param(provincia, Types.VARCHAR),
+            DBUtils.param(seccion, Types.VARCHAR)
+        );
     }
 
+    @Override
     public ResultSet executeQueryGetLastUpdate() throws IOException, SQLException {
-        String sql = "{call [DGTOpendata].[dbo].[Ficheros_GetUltimaFecha]()}";
-
-        Connection connection = dataSource.getConnection();
-
-        var stmt = connection.prepareCall(sql);
-        return stmt.executeQuery();
+        return DBUtils.executeStoredProcedure(
+            dataSource,
+            "[DGTOpendata].[dbo].[Ficheros_GetUltimaFecha]"
+        );
     }
 
+    @Override
     public ResultSet executeQueryGetPermisosAutoescuela(String id_autoescuela) throws IOException, SQLException {
-        String sql = "{call [DGTOpendata].[dbo].[Permisos_GetPermisosByAutoescuela](?)}";
-
-        Connection connection = dataSource.getConnection();
-
-        var stmt = connection.prepareCall(sql);
-        stmt.setString(1, id_autoescuela);
-        return stmt.executeQuery();
+        return DBUtils.executeStoredProcedure(
+            dataSource,
+            "[DGTOpendata].[dbo].[Permisos_GetPermisosByAutoescuela]",
+            DBUtils.param(id_autoescuela, Types.VARCHAR)
+        );
     }
 
+    @Override
     public ResultSet executeQueryGetJefaturasPorAutoescuela(String id_autoescuela) throws IOException, SQLException {
-        String sql = "{call [DGTOpendata].[dbo].[Jefaturas_GetProvinciasYCentros](?)}";
-
-        Connection connection = dataSource.getConnection();
-
-        var stmt = connection.prepareCall(sql);
-        stmt.setString(1, id_autoescuela);
-        return stmt.executeQuery();
+        return DBUtils.executeStoredProcedure(
+            dataSource,
+            "[DGTOpendata].[dbo].[Jefaturas_GetProvinciasYCentros]",
+            DBUtils.param(id_autoescuela, Types.VARCHAR)
+        );
     }
 
-    public ResultSet executeQueryGetExamen(String codigo_autoescuela, String codigo_seccion, String provincia, String centro_examen, String permiso, String tipo_examen, String mes, String anyo, boolean isAddition) throws IOException, SQLException {
-        String sql = "{call [DGTOpendata].[dbo].[Examenes_GetExamenes](?, ?, ?, ?, ?, ?, ?, ?, ?)}";
-
-        Connection connection = dataSource.getConnection();
-
-        var stmt = connection.prepareCall(sql);
-        int paramIndex = 1;
-        if (codigo_autoescuela != null) {
-            stmt.setString(paramIndex++, codigo_autoescuela);
-        } else {
-            stmt.setNull(paramIndex++, java.sql.Types.VARCHAR);
-        }
-        if (codigo_seccion != null) {
-            stmt.setString(paramIndex++, codigo_seccion);
-        } else {
-            stmt.setNull(paramIndex++, java.sql.Types.VARCHAR);
-        }
-        if (provincia != null) {
-            stmt.setString(paramIndex++, provincia);
-        } else {
-            stmt.setNull(paramIndex++, java.sql.Types.VARCHAR);
-        }
-        if (centro_examen != null) {
-            stmt.setString(paramIndex++, centro_examen);
-        } else {
-            stmt.setNull(paramIndex++, java.sql.Types.VARCHAR);
-        }
-        if (permiso != null) {
-            stmt.setString(paramIndex++, permiso);
-        } else {
-            stmt.setNull(paramIndex++, java.sql.Types.VARCHAR);
-        }
-        if (tipo_examen != null) {
-            stmt.setString(paramIndex++, tipo_examen);
-        } else {
-            stmt.setNull(paramIndex++, java.sql.Types.VARCHAR);
-        }
-        if (mes != null) {
-            stmt.setString(paramIndex++, mes);
-        } else {
-            stmt.setNull(paramIndex++, java.sql.Types.VARCHAR);
-        }
-        if (anyo != null) {
-            stmt.setString(paramIndex++, anyo);
-        } else {
-            stmt.setNull(paramIndex++, java.sql.Types.VARCHAR);
-        }
-        stmt.setBoolean(paramIndex++, isAddition);
-        return stmt.executeQuery();
+    @Override
+    public ResultSet executeQueryGetExamen(
+            String codigo_autoescuela,
+            String codigo_seccion,
+            String provincia,
+            String centro_examen,
+            String permiso,
+            String tipo_examen,
+            String mes,
+            String anyo,
+            boolean isAddition
+    ) throws IOException, SQLException {
+        return DBUtils.executeStoredProcedure(
+            dataSource,
+            "[DGTOpendata].[dbo].[Examenes_GetExamenes]",
+            DBUtils.param(codigo_autoescuela, Types.VARCHAR),
+            DBUtils.param(codigo_seccion, Types.VARCHAR),
+            DBUtils.param(provincia, Types.VARCHAR),
+            DBUtils.param(centro_examen, Types.VARCHAR),
+            DBUtils.param(permiso, Types.VARCHAR),
+            DBUtils.param(tipo_examen, Types.VARCHAR),
+            DBUtils.param(mes, Types.VARCHAR),
+            DBUtils.param(anyo, Types.VARCHAR),
+            DBUtils.param(isAddition, Types.BOOLEAN)
+        );
     }
 }
--- src/main/java/com/dgt/opendata/service/utils/DBUtils.java
+++ src/main/java/com/dgt/opendata/service/utils/DBUtils.java
@@ -1,53 +1,84 @@
 package com.dgt.opendata.service.utils;
 
+import javax.sql.DataSource;
+import javax.sql.rowset.CachedRowSet;
+import javax.sql.rowset.RowSetProvider;
+import java.sql.CallableStatement;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import org.springframework.jdbc.core.SqlParameter;
+public final class DBUtils {
 
-public class DBUtils {
+    private DBUtils() {
+        // Utility class
+    }
 
-    private Connection connection;
+    public static class SpParam {
+        private final Object value;
+        private final int sqlType;
 
-    /**
-     * Executes a stored procedure with the given name and parameters.
-     *
-     * @param storedProcedure the name of the stored procedure to execute
-     * @param params a map of parameter names and their corresponding values
-     * @return true if the stored procedure executed successfully
-     * @throws SQLException if a database access error occurs
-     */
-    public boolean executeStoredProcedure(String storedProcedure, SqlParameter[] params) throws SQLException {
-        connection = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=DGTOpendata;encrypt=true;trustServerCertificate=true", "devuser", "devpassword");
-        SqlParameter[] localParams = params == null ? new SqlParameter[0] : params;
-        int paramCount = localParams.length;
-        StringBuilder placeholders = new StringBuilder();
+        public SpParam(Object value, int sqlType) {
+            this.value = value;
+            this.sqlType = sqlType;
+        }
+
+        public Object getValue() {
+            return value;
+        }
+
+        public int getSqlType() {
+            return sqlType;
+        }
+    }
+
+    public static SpParam param(Object value, int sqlType) {
+        return new SpParam(value, sqlType);
+    }
+
+    public static ResultSet executeStoredProcedure(
+            DataSource dataSource,
+            String storedProcedure,
+            SpParam... params
+    ) throws SQLException {
+        String sql = buildCallSql(storedProcedure, params.length);
+
+        try (
+            Connection connection = dataSource.getConnection();
+            CallableStatement stmt = connection.prepareCall(sql)
+        ) {
+            for (int i = 0; i < params.length; i++) {
+                setParameter(stmt, i + 1, params[i]);
+            }
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                CachedRowSet cachedRowSet = RowSetProvider.newFactory().createCachedRowSet();
+                cachedRowSet.populate(rs);
+                return cachedRowSet;
+            }
+        }
+    }
+
+    private static String buildCallSql(String storedProcedure, int paramCount) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{call ").append(storedProcedure).append("(");
+
         for (int i = 0; i < paramCount; i++) {
             if (i > 0) {
-                placeholders.append(", ");
+                sb.append(", ");
             }
-            placeholders.append("?");
+            sb.append("?");
         }
-        String callSql = "{call " + storedProcedure + "(" + placeholders.toString() + ")}";
-        try (PreparedStatement statement = connection.prepareStatement(callSql)) {
-            for (int i = 0; i < paramCount; i++) {
-                SqlParameter param = localParams[i];
-                Object value = null;
-                if (param instanceof org.springframework.jdbc.core.SqlParameterValue) {
-                    value = ((org.springframework.jdbc.core.SqlParameterValue) param).getValue();
-                }
-                if (value != null) {
-                    statement.setObject(i + 1, value);
-                } else {
-                    // If no value provided, bind NULL using the declared SQL type
-                    statement.setNull(i + 1, param.getSqlType());
-                }
-            }
-            statement.execute();
+
+        sb.append(")}");
+        return sb.toString();
+    }
+
+    private static void setParameter(CallableStatement stmt, int index, SpParam param) throws SQLException {
+        if (param.getValue() == null) {
+            stmt.setNull(index, param.getSqlType());
+        } else {
+            stmt.setObject(index, param.getValue(), param.getSqlType());
         }
-        // Do not close the connection here if using connection pooling
-        return true;
     }
 }
